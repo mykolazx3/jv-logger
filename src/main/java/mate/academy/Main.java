77a0ddcf02ffd4477e36651de5ce8fefa66c3e79
Mykola <mykolamalovanchuk@gmail.com>
2024-03-21 13:29:25 +0200

    implement logger

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,8 +6,12 @@ import mate.academy.service.AuthenticationService;
 import mate.academy.service.AuthenticationServiceImpl;
 import mate.academy.service.OrderService;
 import mate.academy.service.OrderServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
@@ -15,6 +19,7 @@ public class Main {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
             e.printStackTrace();
+            logger.error("Can't authenticate user");
             return;
         }
         OrderService orderService = new OrderServiceImpl();
